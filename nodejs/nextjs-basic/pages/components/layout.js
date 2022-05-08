cda24e204f7483158b8b8aca67a43ed04f2791ed
Rojar Smith <rojarsmith@gmail.com>
2022-05-08 19:01:08 +0800

    Add Layout.

---
+++ nodejs/nextjs-basic/pages/components/layout.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+export default function Layout({ children }) {
+    return (
+    <div>
+        {children}
+
+        <footer className={styles.footer}>
+            <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                My First Layout{' '}
+                <span className={styles.logo}>
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                </span>
+            </a>
+        </footer>
+    </div>
+    )
+}
--- nodejs/nextjs-basic/pages/index.js
+++ nodejs/nextjs-basic/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Script from 'next/script'
+import Layout from './components/layout'
 
 export default function Home() {
   return (
@@ -15,84 +16,74 @@ export default function Home() {
 
       <Script src="https://connect.facebook.net/en_US/sdk.js" />
 
-      <main className={styles.main}>
-        <Image src='/images/water.jpg'
-          height={144}
-          width={144}
-        />
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Layout>
+        <main className={styles.main}>
+          <Image src='/images/water.jpg'
+            height={144}
+            width={144}
+          />
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <p className={styles.description}>
+            Get started by editing{' '}
+            <code className={styles.code}>pages/index.js</code>
+          </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h2>Documentation &rarr;</h2>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Learn &rarr;</h2>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-
-          <Link href="./about">
             <a
+              href="https://github.com/vercel/next.js/tree/canary/examples"
               className={styles.card}
             >
-              <h2>About Me &rarr;</h2>
+              <h2>Examples &rarr;</h2>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h2>Deploy &rarr;</h2>
               <p>
                 Instantly deploy your Next.js site to a public URL with Vercel.
               </p>
             </a>
-          </Link>
 
-          <a
-            href="./products/product1"
-            className={styles.card}
-          >
-            <h2>Product1 &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            <Link href="/about">
+              <a
+                className={styles.card}
+              >
+                <h2>About Me &rarr;</h2>
+                <p>
+                  Instantly deploy your Next.js site to a public URL with Vercel.
+                </p>
+              </a>
+            </Link>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+            <a
+              href="./products/product1"
+              className={styles.card}
+            >
+              <h2>Product1 &rarr;</h2>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </main>
+
+      </Layout>
     </div>
   )
 }
