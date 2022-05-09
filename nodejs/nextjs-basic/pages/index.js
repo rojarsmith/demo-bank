1ee97fc8af20b50e299d0c4595b81e2ef427435c
Rojar Smith <rojarsmith@gmail.com>
2022-05-09 10:31:08 +0800

    Add getStaticPaths.

--- nodejs/nextjs-basic/pages/index.js
+++ nodejs/nextjs-basic/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Script from 'next/script'
-import Layout from './components/layout'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
