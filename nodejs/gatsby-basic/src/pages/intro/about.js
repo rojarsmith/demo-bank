18a5a71cffdbc7b37e7679e1f4239e2a1b3e6462
Rojar Smith <rojarsmith@gmail.com>
2022-05-11 20:04:08 +0800

    Add new page.

---
+++ nodejs/gatsby-basic/src/pages/intro/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About</title>
+            <h1>About page.</h1>
+        </main>
+    )
+}
+
+export default AboutPage
