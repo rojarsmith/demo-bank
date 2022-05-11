223a9b9d71b82f3141aee67049833a1e5f06b495
Rojar Smith <rojarsmith@gmail.com>
2022-05-11 21:42:59 +0800

    Add plugins.

--- nodejs/gatsby-basic/gatsby-config.js
+++ nodejs/gatsby-basic/gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
+        // The values for each key in this example are the defaults the plugin uses.
+        sourceMap: true,
+        autoLabel: "dev-only",
+        labelFormat: `[local]`,
+        cssPropOptimization: true,
+      },
+    },
+  ],
 }
