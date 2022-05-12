cc4eb0efe8442c05b3af878c1f8ab323fc15a586
Rojar Smith <rojarsmith@gmail.com>
2022-05-12 08:19:43 +0800

    Css in js-react.

--- nodejs/gatsby-basic/src/pages/components/item1.js
+++ nodejs/gatsby-basic/src/pages/components/item1.js
@@ -1,15 +1,20 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 const Header = styled.div`
 background-color:blue;
 color:red;
 `
 
+const fontSize = css`
+font-size:50px
+`
+
 export default function item1({ children }) {
     return (
         <Header>
-            <h2>Item1</h2>
+            <h2 css={fontSize}>Item1</h2>
         </Header>
     )
 }
