7fceeca2790e80d2e8e3bc420fcf74aef149b9f8
Rojar Smith <rojarsmith@gmail.com>
2022-05-16 11:18:31 +0800

    Add test with the deployment of the new instance.

--- solidity/solidity-basic/test/Inbox.test.js
+++ solidity/solidity-basic/test/Inbox.test.js
@@ -1,12 +1,21 @@
 const Inbox = artifacts.require("Inbox");
 
 contract("Inbox", function (accounts) {
+    console.log('accounts=' + accounts);
     console.log('accounts[0]=' + accounts[0]);
-    const str1 = 'Hi Rojar';
+
+    const str1 = 'Hi Smith';
+
+    let instance;
+
+    beforeEach(async () => {
+        instance = await Inbox.new(str1, { from: accounts[0], gas: 1000000 });
+    });
 
     it(`name should:${str1}`, async function () {
-        let instance = await Inbox.deployed();
+        // let instance = await Inbox.new(str1, {from: accounts[0], gas:1000000});
+        // let instance = await Inbox.deployed();
         let message = await instance.message.call();
-        return assert.equal(message, "Hi Rojar", "message error");
+        return assert.equal(message, "Hi Smith", "message error");
     });
 });
