a1210a7544a945afa9eebc0b8551c5da69a138ab
Rojar Smith <rojarsmith@gmail.com>
2022-06-01 22:26:07 +0800

    Load information from contract to front-end.

--- solidity/solidity-basic-react/src/App.js
+++ solidity/solidity-basic-react/src/App.js
@@ -7,11 +7,18 @@ function App() {
   web3.eth.getAccounts().then(console.log);
 
   const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       let manager = await lottery.methods.manager().call();
+      let players = await lottery.methods.getPlayers().call();
+      let balance = await web3.eth.getBalance(lottery.options.address);
+      console.log();
       setManager(manager);
+      setPlayers(players);
+      setBalance(balance);
     }
 
     fetchData().catch(console.log);
@@ -20,7 +27,9 @@ function App() {
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>This contract is managed by {manager}</p>
+      <p>This contract is managed by {manager}.</p>
+      There are currently {players.length} people entered,
+      competing to win {web3.utils.fromWei(balance, 'ether')} ether!
     </div>
   );
 }
