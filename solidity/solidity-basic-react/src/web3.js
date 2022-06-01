0760492f7216752fb5c4bc761f1054cc250d6491
Rojar Smith <rojarsmith@gmail.com>
2022-06-01 19:53:00 +0800

    Fixed Metamask has deprecated window.web3.currentProvider.

--- solidity/solidity-basic-react/src/web3.js
+++ solidity/solidity-basic-react/src/web3.js
@@ -2,6 +2,10 @@
 // import Web3 from "web3";
 import Web3 from "web3/dist/web3.min.js";
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+}
 
 export default web3;
