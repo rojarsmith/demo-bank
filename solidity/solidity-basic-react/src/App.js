0e702c5b33ccba0a49f0a3757f34e18052597a27
Rojar Smith <rojarsmith@gmail.com>
2022-06-02 21:31:54 +0800

    Add front-end interact with contract.

--- solidity/solidity-basic-react/src/App.js
+++ solidity/solidity-basic-react/src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [manager, setManager] = useState('');
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState('');
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,13 +26,68 @@ function App() {
     fetchData().catch(console.log);
   }, []);
 
+  const onEnter = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage('Waiting on transaction success...');
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether')
+    });
+
+    setMessage('You have been entered!');
+  };
+
+  const onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage('Waiting on transaction success...');
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+
+    });
+
+    setMessage('A winner has bean picked!');
+  }
+
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>This contract is managed by {manager}.</p>
-      There are currently {players.length} people entered,
-      competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      <p>
+        This contract is managed by {manager}.
+        There are currently {players.length} people entered,
+        competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
+
+      <hr />
+
+      <form onSubmit={onEnter}>
+        <h4>Want to try your lock?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+
+      <hr />
+
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={onClick}>Pick a winner!</button>
+      <hr />
+
+      <h1>{message}</h1>
     </div>
+
+
+
   );
 }
 
