d90ce217c8766c43f9debfc1e65aec92530228d5
Rojar Smith <rojarsmith@gmail.com>
2022-06-04 13:01:28 +0800

    A better compile script.

---
+++ solidity/solidity-kickstart/ethereum/compile.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+console.log(buildPath);
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+
+// AssertionError [ERR_ASSERTION]: Invalid callback specified.
+let input = {
+    language: 'Solidity',
+    sources: {
+        'Campaign.sol': {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+// console.log(output);
+// 1 is 1 count of contract
+// const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+    fs.outputJSONSync(
+        path.resolve(buildPath, contract + '.json'),
+        output[contract]
+    );
+}
+
+// `output` here contains the JSON output as specified in the documentation
+for (var contractName in output.contracts['Campaign.sol']) {
+    console.log(
+        contractName +
+        ': ' +
+        output.contracts['Campaign.sol'][contractName].evm.bytecode.object
+    );
+}
---
+++ solidity/solidity-kickstart/ethereum/contracts/Campaign.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.14 <0.9.0;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint256 minimum) public {
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(address(newCampaign));
+    }
+
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint256 value;
+        address recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    mapping(uint256 => Request) public requests;
+    uint256 numRequests;
+    address public manager;
+    uint256 public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint256 public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint256 minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address recipient
+    ) public restricted {
+        Request storage r = requests[numRequests++];
+        r.description = description;
+        r.value = value;
+        r.recipient = recipient;
+        r.complete = false;
+        r.approvalCount = 0;
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!requests[index].approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        payable(request.recipient).transfer(request.value);
+        request.complete = true;
+    }
+}
