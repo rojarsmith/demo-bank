c2996333eb571ee160fd629463e9e2ff49e0cfd5
Rojar Smith <rojarsmith@gmail.com>
2022-06-04 21:49:26 +0800

    Requiring minimum contributions.

--- solidity/solidity-kickstart/test/Campaign.test.js
+++ solidity/solidity-kickstart/test/Campaign.test.js
@@ -32,8 +32,37 @@ describe('My test', function () {
         );
 
     });
+
     it('deploys a factory and a campaign', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+
+        const isContrubutor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContrubutor);
+    });
+
+    it('requires a minimum contribution', async () => {
+        try{
+            await campaign.methods.contribute().send({
+                value:'3',
+                from: accounts[1]
+            });
+            assert(false);
+        }catch(err){
+            console.log('revert');
+            assert(err);
+        }
     });
 });
