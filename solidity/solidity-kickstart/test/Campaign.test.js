ce3b8f36cae96a8b15d765c19f51a50d72b0b388
Rojar Smith <rojarsmith@gmail.com>
2022-06-05 09:50:38 +0800

    End to end test.

--- solidity/solidity-kickstart/test/Campaign.test.js
+++ solidity/solidity-kickstart/test/Campaign.test.js
@@ -5,6 +5,7 @@ const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
+const { send } = require('process');
 
 let accounts;
 let factory;
@@ -77,5 +78,32 @@ describe('My test', function () {
         const request = await campaign.methods.requests(0).call();
 
         assert.equal('Buy box', request.description);
+    });
+
+    it('processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        });
+
+        await campaign.methods
+            .createRequest('A', web3.utils.toWei('3', 'ether'), accounts[1])
+            .send({ from: accounts[0], gas: '1000000' });
+
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gass: '1000000'
+        });
+
+        let balance = await web3.eth.getBalance(accounts[1]);
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance);
+        console.log(balance);
+        assert(balance > 101);
     });
 });
