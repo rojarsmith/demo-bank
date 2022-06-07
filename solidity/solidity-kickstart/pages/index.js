5774916a255f018d40aa3377a425847fa9acbdfa
Rojar Smith <rojarsmith@gmail.com>
2022-06-07 10:39:57 +0800

    The page for listing campaigns.

--- solidity/solidity-kickstart/pages/index.js
+++ solidity/solidity-kickstart/pages/index.js
@@ -1,20 +1,39 @@
 import factory from '../ethereum/factory';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
-export default ({campaigns}) => {
+export default ({ campaigns }) => {
+    const cards = campaigns.map((id) =>
+    (<Card variant="outlined" key={id}>
+        <CardContent>
+            <Typography sx={{ fontSize: 18 }} >
+                {id}
+            </Typography>
+            <Typography sx={{ fontSize: 18 }} >
+                <Link href="#">View Campaign</Link>
+            </Typography>
+        </CardContent>
+    </Card>));
+
     return (
         <>
-            <h1>This is the campaign list page!!!</h1>
-            {campaigns[0]}
+            {cards}
         </>
     );
 };
 
 export async function getServerSideProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
-    return{
-        props:{
-            campaigns
+    console.log(Object.isExtensible(campaigns));
+    let campaignsMock = Object.assign([], campaigns);
+    campaignsMock.push("0xA2A100ac89ce6ce8102E4a8B12a208A8877dFFAA");
+    console.log(campaignsMock);
+    return {
+        props: {
+            campaigns: campaignsMock
         }
     }
 }
