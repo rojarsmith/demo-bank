f42a3eb17f8ed0e344845830a60a2eee9b43e488
Rojar Smith <rojarsmith@gmail.com>
2022-06-08 20:35:54 +0800

    Integrate input adornment and change handlers.

--- solidity/solidity-kickstart/pages/campaigns/new.js
+++ solidity/solidity-kickstart/pages/campaigns/new.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Layout from "../../components/Layout";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function CampaignNew() {
+    const [minimumcontribution, setMinimumcontribution] = useState('');
     return (
         <Layout>
             <Typography
@@ -20,24 +23,27 @@ export default function CampaignNew() {
                     component="div"
                     sx={{ display: { xs: 'none', sm: 'block' } }}
                 >
-                    Minimum Contribution
                 </Typography>
                 <TextField
                     margin="normal"
                     required
                     fullWidth
                     id="coin"
-                    label="coin"
+                    label="Minimum Contribution"
                     name="coin"
                     autoFocus
+                    InputProps={{
+                        endAdornment: <InputAdornment position="start">wei</InputAdornment>,
+                    }}
+                    value={minimumcontribution}
+                    onChange={event => setMinimumcontribution(event.target.value)}
                 />
                 <Button
                     type="submit"
-                    fullWidth
                     variant="contained"
                     sx={{ mt: 3, mb: 2 }}
                 >
-                   Create!
+                    Create!
                 </Button>
             </Box>
         </Layout>
