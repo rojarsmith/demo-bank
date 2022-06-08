035dac2b1761817ebe42642ee89e2f09c6c9997d
Rojar Smith <rojarsmith@gmail.com>
2022-06-08 19:28:49 +0800

    Form creation.

--- solidity/solidity-kickstart/pages/campaigns/new.js
+++ solidity/solidity-kickstart/pages/campaigns/new.js
@@ -1,9 +1,45 @@
 import Layout from "../../components/Layout";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default function CampaignNew() {
     return (
         <Layout>
-            <h1>new campaign</h1>
+            <Typography
+                variant="h3"
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+                Create a Campaign
+            </Typography>
+            <Box component="form">
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"
+                    sx={{ display: { xs: 'none', sm: 'block' } }}
+                >
+                    Minimum Contribution
+                </Typography>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="coin"
+                    label="coin"
+                    name="coin"
+                    autoFocus
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                   Create!
+                </Button>
+            </Box>
         </Layout>
     );
 }
