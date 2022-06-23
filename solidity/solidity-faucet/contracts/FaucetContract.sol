697bf9a9f4fb24c6b9a56cdb5da730295f351a6c
Rojar Smith <rojarsmith@gmail.com>
2022-06-23 16:20:56 +0800

    Storage.

---
+++ solidity/solidity-faucet/contracts/FaucetContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+    uint256 public numOfFunders;
+    mapping(address => bool) private funders;
+
+    receive() external payable {}
+
+    function addFunds() external payable {
+        address funder = msg.sender;
+
+        if (!funders[funder]) {
+            numOfFunders++;
+            funders[funder] = true;
+        }
+    }
+}
+
+// const instance = await Faucet.deployed();
+// instance.addFunds({from: accounts[0], value: "200000000"})
+// instance.addFunds({from: accounts[1], value: "200000000"})
+// instance.getFunderAtIndex(0)
+// instance.getAllFunders()
