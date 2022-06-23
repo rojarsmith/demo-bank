37e25ffd0936a49b10f770452d0c2b7279d4a9f1
Rojar Smith <rojarsmith@gmail.com>
2022-06-23 11:50:17 +0800

    Memory.

--- solidity/solidity-faucet/contracts/Test.sol
+++ solidity/solidity-faucet/contracts/Test.sol
@@ -2,12 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Test {
-    function test(uint256 testNum) external pure returns (uint256) {
+    function test(uint256 testNum) external pure returns (uint256 data) {
         assembly {
             let _num := 4
             let _fmp := mload(0x40)
+            mstore(0x40, 0x90)
         }
 
         return testNum;
+
+        uint8[3] memory items = [1, 2, 3];
+
+        assembly {
+            data := mload(add(0x90, 0x20))
+        }
     }
 }
