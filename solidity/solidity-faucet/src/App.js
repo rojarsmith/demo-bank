7ce8f34631b0f0dd0f293b33d2afbc4d3e4e2a6c
Rojar Smith <rojarsmith@gmail.com>
2022-06-30 12:53:09 +0800

    Web3 integration.

--- solidity/solidity-faucet/src/App.js
+++ solidity/solidity-faucet/src/App.js
@@ -1,15 +1,43 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 import "./App.css";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+
   useEffect(() => {
     const loadProvider = async () => {
-      console.log(window.web3);
-      console.log(window.ethereum);
+      let provider = null;
+
+      if (window.ethereum) {
+
+        provider = window.ethereum;
+
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User denied accounts access!");
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      }
+      else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
     }
 
     loadProvider();
   }, [])
+
+  console.log(web3Api.web3);
 
   return (
     <>
