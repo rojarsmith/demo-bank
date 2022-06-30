05de6cdeea9583291083562639f4026a7992d28d
Rojar Smith <rojarsmith@gmail.com>
2022-06-30 08:59:40 +0800

    Enable ethereum.

--- solidity/solidity-faucet/src/App.js
+++ solidity/solidity-faucet/src/App.js
@@ -1,13 +1,30 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const loadProvider = async () => {
+      console.log(window.web3);
+      console.log(window.ethereum);
+    }
+
+    loadProvider();
+  }, [])
+
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
           <div className="balance-view is-size-2">
-            Current Balance: <string>10</string> ETH
+            Current Balance: <strong>10</strong> ETH
           </div>
+          <button className="btn mr-2"
+            onClick={async () => {
+              const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+              console.log(accounts);
+            }}>
+            Enable Ethereum
+          </button>
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
