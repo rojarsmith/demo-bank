c4c36b91be022d67e0bc6849e0e31d9b98d3f5cb
Rojar Smith <rojarsmith@gmail.com>
2022-07-02 23:50:32 +0800

    Tailwind integration.

--- solidity/solidity-marketplace/pages/index.js
+++ solidity/solidity-marketplace/pages/index.js
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <>Home</>
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+    </>
   )
 }
