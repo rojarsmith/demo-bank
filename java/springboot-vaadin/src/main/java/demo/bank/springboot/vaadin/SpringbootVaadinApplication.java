5ce8d4ec8a389b7abffc34d5dc23a744cd73f83e
Rojar Smith <rojarsmith@gmail.com>
2022-07-08 11:45:44 +0800

    Initial vaadin project.

---
+++ java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/SpringbootVaadinApplication.java
@@ -0,0 +1,13 @@
+package demo.bank.springboot.vaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVaadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVaadinApplication.class, args);
+	}
+
+}
---
+++ java/springboot-vaadin/src/test/java/demo/bank/springboot/vaadin/SpringbootVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.bank.springboot.vaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
