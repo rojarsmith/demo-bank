d1bcdc94d993877eea50cdb323d3d0f466d05278
Rojar Smith <rojarsmith@gmail.com>
2022-07-12 10:46:38 +0800

    Modal content.

--- solidity/solidity-marketplace/pages/marketplace/index.js
+++ solidity/solidity-marketplace/pages/marketplace/index.js
@@ -5,8 +5,10 @@ import { WalletBar } from "@components/ui/web3";
 import { useAccount, useNetwork } from "@components/hooks/web3";
 import { Button } from "@components/ui/common";
 import { OrderModal } from "@components/ui/order";
+import { useState } from "react";
 
 export default function Marketplace({ courses }) {
+    const [selectedCourse, setSelectedCourse] = useState(null)
     const { account } = useAccount();
     const { network } = useNetwork()
 
@@ -31,7 +33,9 @@ export default function Marketplace({ courses }) {
                         course={course}
                         Footer={() =>
                             <div className="mt-4">
-                                <Button variant="lightPurple">
+                                <Button
+                                    onClick={() => setSelectedCourse(course)}
+                                    variant="lightPurple">
                                     Purchase
                                 </Button>
                             </div>
@@ -39,7 +43,12 @@ export default function Marketplace({ courses }) {
                     />
                 }
             </CourseList>
-            <OrderModal />
+            {selectedCourse &&
+                <OrderModal
+                    course={selectedCourse}
+                    onClose={() => setSelectedCourse(null)}
+                />
+            }
         </>
     )
 }
