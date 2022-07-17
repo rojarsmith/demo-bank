e2b51edae967f179aa8de78e226bbd12f8020510
Rojar Smith <rojarsmith@gmail.com>
2022-07-17 23:38:02 +0800

    Active link 2.

--- solidity/solidity-marketplace/components/ui/common/link/index.js
+++ solidity/solidity-marketplace/components/ui/common/link/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router"
 
-export default function ActiveLink({ children, ...props }) {
+export default function ActiveLink({ children, activeLinkClass, ...props }) {
+    const { pathname } = useRouter()
     let className = children.props.className || "";
+    console.log(pathname)
+    console.log(props.href)
+
+    if (pathname === props.href) {
+        // Hard code fix color bug
+        // .replace('text-gray-500 ', ' ')
+        className = `${className} ${activeLinkClass ? activeLinkClass : "text-indigo-600"}`.replace('text-gray-500 ', ' ');
+    }
 
     return (
         <Link {...props}>
