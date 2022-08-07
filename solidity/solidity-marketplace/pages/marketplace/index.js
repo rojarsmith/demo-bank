48434ba4bae767074f0000de09d7157841aff476
Rojar Smith <rojarsmith@gmail.com>
2022-08-07 16:34:45 +0800

    Prepare values.

--- solidity/solidity-marketplace/pages/marketplace/index.js
+++ solidity/solidity-marketplace/pages/marketplace/index.js
@@ -6,13 +6,33 @@ import { Button } from "@components/ui/common";
 import { OrderModal } from "@components/ui/order";
 import { useState } from "react";
 import { MarketHeader } from "@components/ui/marketplace";
+import { useWeb3 } from "@components/providers"
 
 export default function Marketplace({ courses }) {
+    const { web3 } = useWeb3()
+    const { canPurchaseCourse, account } = useWalletInfo()
     const [selectedCourse, setSelectedCourse] = useState(null)
-    const { canPurchaseCourse } = useWalletInfo();
 
-    const purchaseCourse = (order) => {
-        alert(JSON.stringify(order))
+    const purchaseCourse = order => {
+        const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id)
+        console.log(hexCourseId)
+
+        const orderHash = web3.utils.soliditySha3(
+            { type: "bytes16", value: hexCourseId },
+            { type: "address", value: account.data }
+        )
+
+        console.log(orderHash)
+        const emailHash = web3.utils.sha3(order.email)
+
+        console.log(emailHash)
+
+        const proof = web3.utils.soliditySha3(
+            { type: "bytes32", value: emailHash },
+            { type: "bytes32", value: orderHash }
+        )
+
+        console.log(proof)
     }
 
     return (
