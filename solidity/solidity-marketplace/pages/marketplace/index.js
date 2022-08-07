399cd37e615df1711526d40faa6f5b1183b19a75
Rojar Smith <rojarsmith@gmail.com>
2022-08-07 20:55:21 +0800

    Purchase course.

--- solidity/solidity-marketplace/pages/marketplace/index.js
+++ solidity/solidity-marketplace/pages/marketplace/index.js
@@ -9,30 +9,36 @@ import { MarketHeader } from "@components/ui/marketplace";
 import { useWeb3 } from "@components/providers"
 
 export default function Marketplace({ courses }) {
-    const { web3 } = useWeb3()
+    const { web3, contract } = useWeb3();
     const { canPurchaseCourse, account } = useWalletInfo()
     const [selectedCourse, setSelectedCourse] = useState(null)
 
-    const purchaseCourse = order => {
+    const purchaseCourse = async order => {
         const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id)
-        console.log(hexCourseId)
 
         const orderHash = web3.utils.soliditySha3(
             { type: "bytes16", value: hexCourseId },
             { type: "address", value: account.data }
         )
 
-        console.log(orderHash)
         const emailHash = web3.utils.sha3(order.email)
-
-        console.log(emailHash)
 
         const proof = web3.utils.soliditySha3(
             { type: "bytes32", value: emailHash },
             { type: "bytes32", value: orderHash }
         )
 
-        console.log(proof)
+        const value = web3.utils.toWei(String(order.price))
+
+        try {
+            const result = await contract.methods.purchaseCourse(
+                hexCourseId,
+                proof
+            ).send({ from: account.data, value })
+            console.log(result)
+        } catch {
+            console.error("Purchase course: Operation has failed.")
+        }
     }
 
     return (
