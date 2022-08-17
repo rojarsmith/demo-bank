c793f8303988d91342d3855b25dee4310cdb04e7
Rojar Smith <rojarsmith@gmail.com>
2022-08-17 14:07:27 +0800

    Check for no courses.

--- solidity/solidity-marketplace/pages/marketplace/courses/owned.js
+++ solidity/solidity-marketplace/pages/marketplace/courses/owned.js
@@ -5,6 +5,7 @@ import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { getAllCourses } from "@content/courses/fetcher";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function OwnedCourses({ courses }) {
     const router = useRouter();
@@ -13,19 +14,28 @@ export default function OwnedCourses({ courses }) {
 
     return (
         <>
-            <div className="py-4">
-                <MarketHeader />
-            </div>
+            <MarketHeader />
             <section className="grid grid-cols-1">
+                {ownedCourses.hasInitialResponse && (
+                    !ownedCourses.data || ownedCourses?.data.length === 0
+                ) &&
+                    <div className="w-1/2">
+                        <Message type="warning">
+                            <div>You don't own any courses</div>
+                            <Link href="/marketplace">
+                                <a className="font-normal hover:underline">
+                                    <i>Purchase Course</i>
+                                </a>
+                            </Link>
+                        </Message>
+                    </div>
+                }
                 {
                     ownedCourses.data?.map(course =>
                         <OwnedCourseCard
                             key={course.id}
                             course={course}
                         >
-                            {/* <Message>
-                               My custom message!
-                             </Message> */}
                             <Button
                                 onClick={() => router.push(`/courses/${course.slug}`)}
                             >
