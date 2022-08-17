a7fc091c7371c7ca83013578aa1157870a81efba
Rojar Smith <rojarsmith@gmail.com>
2022-08-17 20:27:05 +0800

    Fetch managed courses.

--- solidity/solidity-marketplace/components/providers/web3/hooks/useManagedCourses.js
+++ solidity/solidity-marketplace/components/providers/web3/hooks/useManagedCourses.js
@@ -6,7 +6,8 @@ export const handler = (web3, contract) => account => {
     const swrRes = useSWR(() =>
         (web3 && contract && account) ? `web3/managedCourses/${account}` : null,
         async () => {
-            const courses = [1, 2, 3, 4]
+            const courses = []
+            const courseCount = await contract.methods.getCourseCount().call()
 
             return courses
         }
