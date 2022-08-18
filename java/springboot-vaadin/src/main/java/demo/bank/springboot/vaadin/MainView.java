ff389636dee23db4664c83ac7217b18bfeb80e20
Rojar Smith <rojarsmith@gmail.com>
2022-08-18 13:30:54 +0800

    Add HorizontalLayout.

--- java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/MainView.java
+++ java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/MainView.java
@@ -1,7 +1,10 @@
 package demo.bank.springboot.vaadin;
 
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -18,7 +21,16 @@ public class MainView extends VerticalLayout {
 	private static final long serialVersionUID = -6631199031632258359L;
 
 	public MainView() {
-		add(new Text("Welcome to MainView."));
+		// Use TextField for standard text input
+		TextField textField = new TextField("Your name");
+		textField.addThemeName("bordered");
+
+		// Button click listeners can be defined as lambda expressions
+		Button button = new Button("Say hello");
+
+		HorizontalLayout layout = new HorizontalLayout(textField, button);
+		layout.setDefaultVerticalComponentAlignment(Alignment.END);
+		add(new Text("Welcome to MainView."), layout);
 	}
 
 }
