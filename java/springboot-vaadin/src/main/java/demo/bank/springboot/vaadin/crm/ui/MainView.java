f89715956f0f3824985c35dd32c541254c1d7f8e
Rojar Smith <rojarsmith@gmail.com>
2022-08-20 12:54:22 +0800

    Showing selected contacts in the form

--- java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/crm/ui/MainView.java
+++ java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/crm/ui/MainView.java
@@ -39,11 +39,14 @@ public class MainView extends VerticalLayout {
 		// Gives the component a CSS class name to help with styling.
 		addClassName("list-view");
 		setSizeFull();
-		
+
 		configureFilter();
 		configureGrid();
 
 		form = new ContactForm(companyService.findAll());
+		form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+		form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+		form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
 
 		Div content = new Div(grid, form);
 		content.addClassName("content");
@@ -51,6 +54,20 @@ public class MainView extends VerticalLayout {
 
 		add(filterText, content);
 		updateList();
+
+		closeEditor();
+	}
+
+	private void saveContact(ContactForm.SaveEvent event) {
+		contactService.save(event.getContact());
+		updateList();
+		closeEditor();
+	}
+
+	private void deleteContact(ContactForm.DeleteEvent event) {
+		contactService.delete(event.getContact());
+		updateList();
+		closeEditor();
 	}
 
 	private void configureFilter() {
@@ -70,6 +87,24 @@ public class MainView extends VerticalLayout {
 			return company == null ? "-" : company.getName();
 		}).setHeader("Company");
 		grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+		grid.asSingleSelect().addValueChangeListener(event -> editContact(event.getValue()));
+	}
+
+	public void editContact(Contact contact) {
+		if (contact == null) {
+			closeEditor();
+		} else {
+			form.setContact(contact);
+			form.setVisible(true);
+			addClassName("editing");
+		}
+	}
+
+	private void closeEditor() {
+		form.setContact(null);
+		form.setVisible(false);
+		removeClassName("editing");
 	}
 
 	private void updateList() {
