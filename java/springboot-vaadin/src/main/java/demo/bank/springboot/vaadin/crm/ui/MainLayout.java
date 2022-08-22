a4c31906c9abf6aa639ceefa06c0267924529d15
Rojar Smith <rojarsmith@gmail.com>
2022-08-22 17:08:23 +0800

    Adding a logout link

--- java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/crm/ui/MainLayout.java
+++ java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/crm/ui/MainLayout.java
@@ -3,6 +3,7 @@ package demo.bank.springboot.vaadin.crm.ui;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -31,11 +32,15 @@ public class MainLayout extends AppLayout {
 	private void createHeader() {
 		H1 logo = new H1("Vaadin CRM");
 		logo.addClassName("logo");
-		HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
 
+		Anchor logout = new Anchor("logout", "Log out");
+
+		HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logout);
+		header.expand(logo);
 		header.setDefaultVerticalComponentAlignment(Alignment.CENTER);
 		header.setWidth("100%");
 		header.addClassName("header");
+
 		addToNavbar(header);
 	}
 
