e7209fd82dbff3b2b9a82f053e3c2bd69aaedd4a
Rojar Smith <rojarsmith@gmail.com>
2022-08-23 08:28:58 +0800

    Turning a Vaadin Flow Application Into an Installable PWA

--- java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/SpringbootVaadinApplication.java
+++ java/springboot-vaadin/src/main/java/demo/bank/springboot/vaadin/SpringbootVaadinApplication.java
@@ -5,8 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class SpringbootVaadinApplication extends SpringBootServletInitializer {
+@PWA(name = "VaadinCRM", shortName = "CRM", offlinePath = "offline.html", offlineResources = { "./styles/offline.css",
+		"./images/offline.png" })
+public class SpringbootVaadinApplication extends SpringBootServletInitializer implements AppShellConfigurator {
+
+	private static final long serialVersionUID = -8380160364189618667L;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootVaadinApplication.class, args);
