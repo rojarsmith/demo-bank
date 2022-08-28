7b17a1c222a4406ecb31af7e1d366d3e6179b7af
Rojar Smith <rojarsmith@gmail.com>
2022-08-28 13:12:42 +0800

    Verification input

--- solidity/solidity-marketplace/pages/marketplace/courses/managed.js
+++ solidity/solidity-marketplace/pages/marketplace/courses/managed.js
@@ -1,19 +1,58 @@
 import { useAccount, useManagedCourses } from "@components/hooks/web3";
+import { useWeb3 } from "@components/providers";
 import { Button } from "@components/ui/common";
 import { CourseFilter, ManagedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { useState } from "react";
 
-export default function ManageCourses() {
+const VerificationInput = ({ onVerify }) => {
     const [email, setEmail] = useState("")
+
+    return (
+        <div className="flex mr-2 relative rounded-md">
+            <input
+                value={email}
+                onChange={({ target: { value } }) => setEmail(value)}
+                type="text"
+                name="account"
+                id="account"
+                className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
+                placeholder="0x2341ab..." />
+            <Button
+                onClick={() => {
+                    onVerify(email)
+                }}
+            >
+                Verify
+            </Button>
+        </div>
+    )
+}
+
+export default function ManageCourses() {
+    const [proofedOwnership, setProofedOwnership] = useState({})
+    const { web3 } = useWeb3()
     const { account } = useAccount()
     const { managedCourses } = useManagedCourses(account.data)
 
     const verifyCourse = (email, { hash, proof }) => {
-        console.log(email)
-        console.log(hash)
-        console.log(proof)
+        debugger
+        const emailHash = web3.utils.sha3(email)
+        const proofToCheck = web3.utils.soliditySha3(
+            { type: "bytes32", value: emailHash },
+            { type: "bytes32", value: hash }
+        )
+
+        proofToCheck === proof ?
+            setProofedOwnership({
+                ...proofedOwnership,
+                [hash]: true
+            }) :
+            setProofedOwnership({
+                ...proofedOwnership,
+                [hash]: false
+            })
     }
 
     return (
@@ -26,26 +65,14 @@ export default function ManageCourses() {
                         key={course.ownedCourseId}
                         course={course}
                     >
-                        <div className="flex mr-2 relative rounded-md">
-                            <input
-                                value={email}
-                                onChange={({ target: { value } }) => setEmail(value)}
-                                type="text"
-                                name="account"
-                                id="account"
-                                className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
-                                placeholder="0x2341ab..." />
-                            <Button
-                                onClick={() => {
-                                    verifyCourse(email, {
-                                        hash: course.hash,
-                                        proof: course.proof
-                                    })
-                                }}
-                            >
-                                Verify
-                            </Button>
-                        </div>
+                        <VerificationInput
+                            onVerify={email => {
+                                verifyCourse(email, {
+                                    hash: course.hash,
+                                    proof: course.proof
+                                })
+                            }}
+                        />
                     </ManagedCourseCard>
                 )}
             </section>
