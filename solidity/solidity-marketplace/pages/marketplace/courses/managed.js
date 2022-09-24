90159de3338950b1e4e2c523548de9080eb8f194
Rojar Smith <rojarsmith@gmail.com>
2022-09-24 13:54:53 +0800

    Search input value

--- solidity/solidity-marketplace/pages/marketplace/courses/managed.js
+++ solidity/solidity-marketplace/pages/marketplace/courses/managed.js
@@ -73,6 +73,14 @@ export default function ManageCourses() {
         changeCourseState(courseHash, "deactivateCourse")
     }
 
+    const searchCourse = courseHash => {
+        if (!courseHash) {
+            return
+        }
+
+        alert(courseHash)
+    }
+
     if (!account.isAdmin) {
         return null
     }
@@ -80,7 +88,9 @@ export default function ManageCourses() {
     return (
         <>
             <MarketHeader />
-            <CourseFilter />
+            <CourseFilter
+                onSearchSubmit={searchCourse}
+            />
             <section className="grid grid-cols-1">
                 {managedCourses.data?.map(course =>
                     <ManagedCourseCard
