5e91e0b12060b5ccd4efbd3a50a0390b1df927d1
Rojar Smith <rojarsmith@gmail.com>
2022-09-24 16:01:01 +0800

    Display filtered courses

--- solidity/solidity-marketplace/pages/marketplace/courses/managed.js
+++ solidity/solidity-marketplace/pages/marketplace/courses/managed.js
@@ -139,13 +139,19 @@ export default function ManageCourses() {
         )
     }
 
-    useEffect(() => {
-        console.log(filters)
-    }, [filters])
-
     if (!account.isAdmin) {
         return null
     }
+
+    const filteredCourses = managedCourses.data
+        ?.filter((course) => {
+            if (filters.state === "all") {
+                return true
+            }
+
+            return course.state === filters.state
+        })
+        .map(course => renderCard(course))
 
     return (
         <>
@@ -162,7 +168,12 @@ export default function ManageCourses() {
                     </div>
                 }
                 <h1 className="text-2xl font-bold p-5">All Courses</h1>
-                {managedCourses.data?.map(course => renderCard(course))}
+                {filteredCourses}
+                {filteredCourses?.length === 0 &&
+                    <Message type="warning">
+                        No courses to display
+                    </Message>
+                }
             </section>
         </>
     )
