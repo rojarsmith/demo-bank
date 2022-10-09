f926c455de8c515c940a7b58b23fa4e8fa3cac51
Rojar Smith <rojarsmith@gmail.com>
2022-10-09 15:15:41 +0800

    Implement transfer NFTs

--- solidity/solidity-nft-marketplace/src/contracts/ERC721.sol
+++ solidity/solidity-nft-marketplace/src/contracts/ERC721.sol
@@ -14,6 +14,9 @@ contract ERC721 {
     // Mapping from owner to number of owned tokens
     mapping(address => uint256) private _OwnedTokensCount;
 
+    // Mapping from token id to approved addresses
+    mapping(uint256 => address) private _tokenApprovals;
+
     function balanceOf(address _owner) public view returns (uint256) {
         require(_owner != address(0), "owner query for non-existent token");
         return _OwnedTokensCount[_owner];
@@ -24,7 +27,7 @@ contract ERC721 {
     ///  about them do throw.
     /// @param _tokenId The identifier for an NFT
     /// @return The address of the owner of the NFT
-    function ownerOf(uint256 _tokenId) external view returns (address) {
+    function ownerOf(uint256 _tokenId) public view returns (address) {
         address owner = _tokenOwner[_tokenId];
         require(owner != address(0), "owner query for non-existent token");
         return owner;
@@ -42,5 +45,45 @@ contract ERC721 {
         _OwnedTokensCount[to] += 1;
 
         emit Transfer(address(0), to, tokenId);
+    }
+
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal {
+        require(
+            _to != address(0),
+            "Error - ERC721 Transfer to the zero address"
+        );
+        require(
+            ownerOf(_tokenId) == _from,
+            "Trying to transfer a token the address does not own!"
+        );
+
+        _OwnedTokensCount[_from] -= 1;
+        _OwnedTokensCount[_to] += 1;
+
+        _tokenOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public {
+        _transferFrom(_from, _to, _tokenId);
     }
 }
