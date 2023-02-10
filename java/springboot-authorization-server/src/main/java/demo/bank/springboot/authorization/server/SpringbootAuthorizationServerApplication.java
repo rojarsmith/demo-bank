5cd1b466a7cc49562f33395d2016ccf32b604392
Rojar Smith <rojarsmith@gmail.com>
2023-02-11 00:52:51 +0800

    Initial database data.

--- java/springboot-authorization-server/src/main/java/demo/bank/springboot/authorization/server/SpringbootAuthorizationServerApplication.java
+++ java/springboot-authorization-server/src/main/java/demo/bank/springboot/authorization/server/SpringbootAuthorizationServerApplication.java
@@ -2,11 +2,13 @@ package demo.bank.springboot.authorization.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringbootAuthorizationServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("rojarsmith=" + new BCryptPasswordEncoder().encode("rojarsmith"));
 		SpringApplication.run(SpringbootAuthorizationServerApplication.class, args);
 	}
 
