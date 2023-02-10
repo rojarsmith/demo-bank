5c6368d1c00f16646c17f0378ba51344f5261177
Rojar Smith <rojarsmith@gmail.com>
2023-02-10 21:26:23 +0800

    Begin Spring Boot Authorization Server

---
+++ java/springboot-authorization-server/src/main/java/demo/bank/springboot/authorization/server/SpringbootAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package demo.bank.springboot.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ java/springboot-authorization-server/src/test/java/demo/bank/springboot/authorization/server/SpringbootAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.bank.springboot.authorization.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
