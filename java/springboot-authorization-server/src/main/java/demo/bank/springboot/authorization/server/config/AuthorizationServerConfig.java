e22218ba98ad77277273da79c411330ba1d29448
Rojar Smith <rojarsmith@gmail.com>
2023-02-12 14:29:18 +0800

    Integrate JWK.

--- java/springboot-authorization-server/src/main/java/demo/bank/springboot/authorization/server/config/AuthorizationServerConfig.java
+++ java/springboot-authorization-server/src/main/java/demo/bank/springboot/authorization/server/config/AuthorizationServerConfig.java
@@ -1,7 +1,12 @@
 package demo.bank.springboot.authorization.server.config;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +32,12 @@ import org.springframework.security.oauth2.server.authorization.settings.TokenSe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
 /**
  * @author Rojar Smith
  *
@@ -40,11 +51,13 @@ public class AuthorizationServerConfig {
      * Default Ebdpoints
      *
      * <p>
-     * Authorization /oauth2/authorize Token Endpoint /oauth2/token Token Revocation
-     * /oauth2/revoke Token Introspection /oauth2/introspect JWK Set Ecdpoint
-     * /oauth2/jwks Authorization Server Metadata
-     * /.well-known/oauth-authorization-server OIDC Provider Configuration
-     * /.well-known/openid-configuration
+     * Authorization /oauth2/authorize 
+     * Token Endpoint /oauth2/token
+     * Token Revocation /oauth2/revoke 
+     * Token Introspection /oauth2/introspect 
+     * JWK Set Ecdpoint /oauth2/jwks 
+     * Authorization Server Metadata /.well-known/oauth-authorization-server 
+     * OIDC Provider Configuration /.well-known/openid-configuration
      */
     @Bean
     @Order(1)
@@ -112,6 +125,17 @@ public class AuthorizationServerConfig {
     }
 
     @Bean
+    JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    @Bean
     EmbeddedDatabase embeddedDatabase() {
         return new EmbeddedDatabaseBuilder() //
                 .setType(EmbeddedDatabaseType.H2).setScriptEncoding("UTF-8") //
@@ -121,6 +145,18 @@ public class AuthorizationServerConfig {
                 .addScript(
                         "org/springframework/security/oauth2/server/authorization/client/oauth2-registered-client-schema.sql")
                 .build();
+    }
+
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
     }
 
 }
